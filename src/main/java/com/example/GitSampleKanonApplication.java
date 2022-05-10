4ad0924cd9621e5d2003579ed210565986472cb5
tanakakanon <kanon.tanaka@rakus-partners.co.jp>
2022-05-10 13:42:08 +0900

    first commit

---
+++ src/main/java/com/example/GitSampleKanonApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleKanonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleKanonApplication.class, args);
+	}
+
+}
